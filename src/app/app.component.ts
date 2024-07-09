8d2bb5cc0b788578eaf5a14b4582682816238083
Dmitry Balaev <d.balaev@yadro.com>
2024-07-09 11:43:31 +0300

    Add prettier formatting. Scss migration.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,23 @@
+import { HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
-import { HttpClientModule} from "@angular/common/http";
 import { CurrancyRatesHttpService } from './currancy-rates.http.service';
 
-
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [HttpClientModule],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
-  providers: [CurrancyRatesHttpService]
+    selector: 'app-root',
+    standalone: true,
+    imports: [HttpClientModule],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
+    providers: [CurrancyRatesHttpService],
 })
 export class AppComponent {
-  constructor(private httpService: CurrancyRatesHttpService){
-  }
-  ngOnInit(){
-    
-    this.httpService.getCurrencyRates().subscribe({next:(data:any) => {
-        
-    }
-    });
-   
-}
-    
-}
+    constructor(private httpService: CurrancyRatesHttpService) {}
 
+    ngOnInit() {
+        this.httpService.getCurrencyRates().subscribe({
+            next: (data) => {
+                console.log(data);
+            },
+        });
+    }
+}
--- src/app/currancy-rates.http.service.ts
+++ src/app/currancy-rates.http.service.ts
@@ -1,25 +1,28 @@
-import {Injectable} from "@angular/core";
-import {HttpClient} from "@angular/common/http";
-import { map, catchError, tap} from "rxjs/operators";
-import { Observable } from "rxjs";
-import { CurrencyRate } from "./currency-rate.model";
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { CurrencyRate } from './currency-rate.model';
 
 @Injectable()
-export class CurrancyRatesHttpService{
-    readonly apiUrl ='https://openexchangerates.org/api/latest.json?app_id=90f7e3e0677d4de29f812687ad7f0764&base=USD&symbols=USD,GBR,EUR,RUB' 
-    errorMessage: String = "";
-    constructor(private http: HttpClient){ }
-         
-    getCurrencyRates(): Observable<CurrencyRate[]>{
-        return this.http.get(this.apiUrl).pipe(map((info:any)=>{
-            let currencyRatesList : CurrencyRate[] =[]
-            for (var key in info.rates) {
-                currencyRatesList.push(
-                    {name: key,rate: info.rates[key]})
-            }
-            console.log(currencyRatesList );
-            return currencyRatesList     
-    }))
-}
-    
+export class CurrancyRatesHttpService {
+    readonly apiUrl =
+        'https://openexchangerates.org/api/latest.json?app_id=90f7e3e0677d4de29f812687ad7f0764&base=USD&symbols=USD,GBR,EUR,RUB';
+
+    constructor(private http: HttpClient) {}
+
+    getCurrencyRates(): Observable<CurrencyRate[]> {
+        return this.http.get(this.apiUrl).pipe(
+            map((info: any) => {
+                let currencyRatesList: CurrencyRate[] = [];
+                for (var key in info.rates) {
+                    currencyRatesList.push({
+                        name: key,
+                        rate: info.rates[key],
+                    });
+                }
+                return currencyRatesList;
+            }),
+        );
+    }
 }
