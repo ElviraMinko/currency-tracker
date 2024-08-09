17fc16c857f4881ad5af6458741a103353e19419
ElviraMinko <e.minko@yadro.com>
2024-08-09 15:08:27 +0300

    change transloco path

--- src/app/transloco-loader.ts
+++ src/app/transloco-loader.ts
@@ -8,6 +8,6 @@ export class TranslocoHttpLoader implements TranslocoLoader {
 
     getTranslation(lang: string) {
         return this.http.get<Translation>(`
-            /assets/i18n/${lang}.json`);
+            ./assets/i18n/${lang}.json`);
     }
 }
